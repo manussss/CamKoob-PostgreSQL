91e163cf694771ee4716b9bf0acc4fcc64224453
ebranquinho <branquinhomanu@gmail.com>
2025-05-24 16:10:21 -0300

    update order repository

--- CamKoob.PostgreSQL.Infra/Repositories/OrderRepository.cs
+++ CamKoob.PostgreSQL.Infra/Repositories/OrderRepository.cs
@@ -7,60 +7,61 @@ public class OrderRepository(IConfiguration configuration) : IOrderRepository
         using var conn = new NpgsqlConnection(configuration.GetConnectionString("Database"));
 
         var sql = @"
-            INSERT INTO orders (id, order)
-            VALUES (@Id, to_jsonb(@Order::json))
+            INSERT INTO orders (id, content)
+            VALUES (@id, to_jsonb(@content::json))
         ";
 
-        await conn.ExecuteAsync(sql, new { id = order.Id, order = order });
+        await conn.ExecuteAsync(sql, new { id = order.Id, content = JsonSerializer.Serialize(order) });
     }
 
-    public async Task<IEnumerable<Order>> GetAsync()
+    public async Task<IEnumerable<Order>?> GetAsync()
     {
         using var conn = new NpgsqlConnection(configuration.GetConnectionString("Database"));
 
-        var sql = "SELECT order FROM orders";
+        var sql = "SELECT content FROM Orders";
         var orders = await conn.QueryAsync<string>(sql);
 
-        return orders.Select(json => System.Text.Json.JsonSerializer.Deserialize<Order>(json)!);
+        if (orders is null || !orders.Any())
+            return null;
+
+        return orders.Select(json => JsonSerializer.Deserialize<Order>(json)!);
     }
 
     public async Task<Order?> GetByIdAsync(Guid id)
     {
         using var conn = new NpgsqlConnection(configuration.GetConnectionString("Database"));
 
-        var sql = "SELECT order FROM orders WHERE id = @id";
+        var sql = "SELECT content FROM orders WHERE id = @id";
         var json = await conn.QueryFirstOrDefaultAsync<string>(sql, new { id = id });
 
         if (string.IsNullOrEmpty(json))
             return null;
 
-        return System.Text.Json.JsonSerializer.Deserialize<Order>(json);
+        return JsonSerializer.Deserialize<Order>(json);
     }
 
-    public async Task<bool> UpdateAsync(Guid id, Order newOrder)
+    public async Task UpdateAsync(Guid id, Order newOrder)
     {
         using var conn = new NpgsqlConnection(configuration.GetConnectionString("Database"));
 
-        var selectSql = "SELECT order FROM orders WHERE id = @id";
+        var selectSql = "SELECT content FROM orders WHERE id = @id";
         var json = await conn.QueryFirstOrDefaultAsync<string>(selectSql, new { id = id });
 
         if (json is null)
-            return false;
+            return;
 
-        var order = System.Text.Json.JsonSerializer.Deserialize<Order>(json)!;
+        var order = JsonSerializer.Deserialize<Order>(json)!;
 
         if (!string.IsNullOrWhiteSpace(newOrder.Description))
             order.SetDescription(newOrder.Description);
 
         if (newOrder.OrderItems != null && newOrder.OrderItems.Count != 0)
-            order.AddItems(newOrder.OrderItems);
+            order.SetItems(newOrder.OrderItems);
 
         var updateSql = @"
-            UPDATE pedidos SET conteudo = to_jsonb(@order::json) WHERE id = @Id
+            UPDATE orders SET content = to_jsonb(@content::json) WHERE id = @id
         ";
 
-        await conn.ExecuteAsync(updateSql, new { id = id, order = order });
-
-        return true;
+        await conn.ExecuteAsync(updateSql, new { id = id, content = JsonSerializer.Serialize(order) });
     }
 }
--- CamKoob.PostgreSQL.Infra/Usings.cs
+++ CamKoob.PostgreSQL.Infra/Usings.cs
@@ -2,3 +2,4 @@ global using CamKoob.PostgreSQL.Domain.OrdersAggregate;
 global using Dapper;
 global using Microsoft.Extensions.Configuration;
 global using Npgsql;
+global using System.Text.Json;
